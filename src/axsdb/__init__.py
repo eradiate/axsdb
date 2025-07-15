aa92972315f152262057b2393a0f2cf603ff9db2
Vincent Leroy <vincent.leroy@rayference.eu>
2025-07-15 14:00:35 +0200

    Rename _factory → factory

--- src/axsdb/__init__.py
+++ src/axsdb/__init__.py
@@ -4,8 +4,8 @@ from .core import (
     ErrorHandlingConfiguration,
     MonoAbsorptionDatabase,
 )
-from ._factory import AbsorptionDatabaseFactory
 from .error import get_error_handling_config, set_error_handling_config
+from .factory import AbsorptionDatabaseFactory
 from ._version import version as __version__
 
 __all__ = [
