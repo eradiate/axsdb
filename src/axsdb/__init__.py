6611987308157f8bbd3262f90e47c2d06c20a3ee
Vincent Leroy <vincent.leroy@rayference.eu>
2025-07-15 12:05:03 +0200

    Add factory

--- src/axsdb/__init__.py
+++ src/axsdb/__init__.py
@@ -4,11 +4,13 @@ from .core import (
     ErrorHandlingConfiguration,
     MonoAbsorptionDatabase,
 )
+from ._factory import AbsorptionDatabaseFactory
 from .error import get_error_handling_config, set_error_handling_config
 from ._version import version as __version__
 
 __all__ = [
     "AbsorptionDatabase",
+    "AbsorptionDatabaseFactory",
     "ErrorHandlingConfiguration",
     "CKDAbsorptionDatabase",
     "MonoAbsorptionDatabase",
