c26b1882d410717819bbb3a42551d239b4395bae
Vincent Leroy <vincent.leroy@rayference.eu>
2025-07-15 14:01:40 +0200

    Expose all error utility components in top-level namespace

--- src/axsdb/__init__.py
+++ src/axsdb/__init__.py
@@ -1,17 +1,24 @@
 from .core import (
     AbsorptionDatabase,
     CKDAbsorptionDatabase,
-    ErrorHandlingConfiguration,
     MonoAbsorptionDatabase,
 )
-from .error import get_error_handling_config, set_error_handling_config
 from .factory import AbsorptionDatabaseFactory
+from .error import (
+    get_error_handling_config,
+    set_error_handling_config,
+    ErrorHandlingAction,
+    ErrorHandlingPolicy,
+    ErrorHandlingConfiguration,
+)
 from ._version import version as __version__
 
 __all__ = [
     "AbsorptionDatabase",
     "AbsorptionDatabaseFactory",
+    "ErrorHandlingAction",
     "ErrorHandlingConfiguration",
+    "ErrorHandlingPolicy",
     "CKDAbsorptionDatabase",
     "MonoAbsorptionDatabase",
     "get_error_handling_config",
