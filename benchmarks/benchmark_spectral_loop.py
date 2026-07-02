feae3e8ad899d66be453dc406cc5af485cfdc1cf
Vincent Leroy <vincent.leroy@rayference.eu>
2026-07-02 19:04:49 +0200

    Add spectral loop benchmark, increase benchmark verbosity

---
+++ benchmarks/benchmark_spectral_loop.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+import xarray as xr
+from scipy.special import roots_sh_legendre
+
+import axsdb
+from axsdb import CKDAbsorptionDatabase
+
+ROOT_DIR = Path(__file__).parent.parent
+
+ureg = axsdb.units.get_unit_registry()
+db = CKDAbsorptionDatabase.from_directory(ROOT_DIR / "benchmarks/data/nanockd_v1")
+
+ws = db.spectral_coverage.index.get_level_values("wavelength [nm]").to_numpy() * ureg.nm
+gs, _ = roots_sh_legendre(16)
+thermoprops_us_standard = xr.load_dataset(
+    ROOT_DIR / "tests/data/afgl_1986-us_standard.nc"
+)
+
+
+class BenchmarkSpectralLoop:
+    def main(self):
+        for w in ws:
+            for g in gs:
+                db.eval_sigma_a_ckd(w, g, thermoprops=thermoprops_us_standard)
+
+    def benchmark_main(self, benchmark):
+        benchmark(self.main)
+
+
+if __name__ == "__main__":
+    BenchmarkSpectralLoop().main()
